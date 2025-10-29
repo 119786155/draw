baa190d1434c876f234a59ecac3fdc078b4f29a0
119786155 <119786155@qq.com>
2025-10-29 22:38:07 +0800

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Excalidraw } from "@excalidraw/excalidraw"
+import "@excalidraw/excalidraw/index.css"
+
+function App() {
+  return (
+    <div style={{ height: "100dvh" }}>
+      <Excalidraw />
+    </div>
+  )
+}
+
+export default App
