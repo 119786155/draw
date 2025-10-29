c268c3bfcdd6482fba28cdda9bcbffec8c4b9857
119786155 <119786155@qq.com>
2025-10-29 23:01:54 +0800

    support dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import { Excalidraw } from "@excalidraw/excalidraw"
 import "@excalidraw/excalidraw/index.css"
 
 function App() {
+
+	const themeDarkQuery = window.matchMedia('(prefers-color-scheme: dark)')
+  const [theme, setTheme] = useState(themeDarkQuery.matches ? 'dark' : 'light')
+
+	themeDarkQuery.addEventListener('change', mediaQueryListEvent => {
+		setTheme(mediaQueryListEvent.matches ? 'dark' : 'light')
+	})
+
   return (
     <div style={{ height: "100dvh" }}>
-      <Excalidraw />
+      <Excalidraw theme={theme} />
     </div>
   )
 }
